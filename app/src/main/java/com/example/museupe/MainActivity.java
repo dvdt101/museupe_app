6be1203239c0a3452e64e12d17cb3025c1693b8a
David Tavares <davidtavaresfs@gmail.com>
2021-02-24 15:03:19 -0300

    Adicionando projeto

---
+++ app/src/main/java/com/example/museupe/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.museupe;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void localMuseuGoogleMaps (View v){
+
+        Uri uriGeografico = Uri.parse("geo:0, 0?q=Museu+do+estado,Recife");
+        Intent open = new Intent(Intent.ACTION_VIEW, uriGeografico);
+        startActivity(open);
+    }
+
+    public void openWebSite (View v){
+
+        Uri siteUrl =  Uri.parse("http://www.museudoestadope.com.br");
+        Intent open = new Intent(Intent.ACTION_VIEW, siteUrl);
+        startActivity(open);
+    }
+
+    public void openAgendar(View v){
+        Intent open = new Intent(this, agendarActivity.class);
+        startActivity(open);
+    }
+
+    public void openDonate(View v){
+
+    }
+
+}
---
+++ app/src/main/java/com/example/museupe/agendarActivity.java
@@ -0,0 +1,17 @@
+package com.example.museupe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class agendarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agendar);
+
+    }
+}
